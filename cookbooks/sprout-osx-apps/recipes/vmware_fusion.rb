63cab147c57332887287d4ab87f3c3b0a6dd63e5
Sanjay Modi <sanjaymodi@new-host-3.home>
2013-08-29 13:19:50 -0400

    update vm

--- cookbooks/sprout-osx-apps/recipes/vmware_fusion.rb
+++ cookbooks/sprout-osx-apps/recipes/vmware_fusion.rb
@@ -1,7 +1,8 @@
 dmg_package "VMWare Fusion" do
+  ###
   #source "http://download2us.softpedia.com/dl/017d316f803d0eb4e0767709c12cdf6f/5217c91f/400024122/mac/System-Utilities/VMware-Fusion-5.0.3-1040386-light.dmg"
-  source "https://dl.dropboxusercontent.com/sh/llqs1rkjguq7n6d/IZvooSa5eO/VMware-Fusion-5.0.3-1040386-light.dmg?token_hash=AAF17QH5r6CWUSStAwX3EzngIdnnuY12eP57G_RP5mSvAA&dl=1"
-action :install
+  source  "https://dl.dropboxusercontent.com/sh/llqs1rkjguq7n6d/IZvooSa5eO/VMware-Fusion-5.0.3-1040386-light.dmg?token_hash=AAF17QH5r6CWUSStAwX3EzngIdnnuY12eP57G_RP5mSvAA&dl=1"
+  action :install
   type "app"
   package_id "com.vmware.tools.application"
 end
